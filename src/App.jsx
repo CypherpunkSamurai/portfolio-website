a0d86f59926414e81d6d6ca211e89fe24d563a96
CypherpunkSamurai <CypherpunkSamurai@users.noreply.github.com>
2022-08-11 18:19:02 +0530

    make the image corners rounded and overflow hide

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,7 @@ function App() {
             
 
             <Flex borderRadius={'5px'} flexDirection='column' alignItems={'center'} color={bgColor[!colorMode]} p='5px' maxW={['80%','60%']} my={'15px'}>
-              <Box overflow='hidden'>
+              <Box overflow='hidden' borderRadius={'lg'}>
                 <Image src="https://www.blendernation.com/wp-content/uploads/2016/02/JapaneseVoxelGarden_1.jpg"/>
               </Box>
             </Flex>
